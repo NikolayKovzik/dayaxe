0f1e409323d25975f0e87d26313a364ed208aee1
Nikolay <nikolay.kovzik@gmail.com>
2023-02-20 15:39:55 +0300

    import logos from figma & create hotels section markup

---
+++ src/scripts/HotelsSlider.ts
@@ -0,0 +1,34 @@
+// export default class HotelsSlider {
+//   private srcArr: string[];
+
+//   private container: HTMLElement;
+
+//   constructor() {
+//     this.srcArr = [
+//       './assets/images/hotels/the_ritz_carlton.png',
+//       './assets/images/hotels/hyatt.png',
+//       './assets/images/hotels/viceroy.png',
+//       './assets/images/hotels/fairmont.png',
+//       './assets/images/hotels/four_seasons.png',
+//       './assets/images/hotels/hilton.png',
+//       './assets/images/hotels/westin.png',
+//       './assets/images/hotels/intercontinental.png',
+//       './assets/images/hotels/the_ritz_carlton2.png',
+//       './assets/images/hotels/hyatt2.png',
+//       './assets/images/hotels/viceroy2.png',
+//       './assets/images/hotels/fairmont2.png',
+//       './assets/images/hotels/four_seasons2.png',
+//       './assets/images/hotels/hilton2.png',
+//       './assets/images/hotels/westin2.png',
+//       './assets/images/hotels/intercontinental2.png',
+//     ];
+//     this.container = document.querySelector('.hotels__list');
+//   }
+
+//   init() {}
+
+//   createElement() {
+
+//   }
+
+// }
